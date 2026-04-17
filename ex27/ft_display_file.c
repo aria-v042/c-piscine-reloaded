82ca722cd9cef1b4ec3424493d3f896cd0614a93
aria <redcore_one@proton.me>
2026-04-17 10:14:56 +0100

    ex27: add files to turn in

---
+++ ex27/ft_display_file.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 10:10:05 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/04/17 10:11:12 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h> 
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+void	ft_display_file(char *string)
+{
+	int		file_ptr;
+	char	c;
+
+	file_ptr = open(string, O_RDONLY, 0);
+	if (file_ptr == -1)
+		ft_putstr("Cannot read file.\n");
+	while (read(file_ptr, &c, 1) == 1)
+		ft_putchar(c);
+	close(file_ptr);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		ft_putstr("File name missing.\n");
+	if (argc == 2)
+		ft_display_file(argv[1]);
+	if (argc > 2)
+		ft_putstr("Too many arguments.\n");
+	return (0);
+}
