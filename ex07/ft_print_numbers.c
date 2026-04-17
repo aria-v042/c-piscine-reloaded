7ffebc48761c62eefd6b03de0b64d8473a71a27e
aria <redcore_one@proton.me>
2026-04-17 05:24:53 +0100

    ex07: add files to turn in

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frodrig2 <frodrig2@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 04:41:45 by frodrig2          #+#    #+#             */
+/*   Updated: 2026/04/17 05:21:13 by frodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+//void	ft_putchar(char c)
+//{
+//	write(1, &c, 1);
+//}
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	char	numchar;
+
+	numchar = '0';
+	while (numchar <= '9')
+	{
+		ft_putchar();
+		numchar++;_
+	}
+}
+
+// /*
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
+// */
